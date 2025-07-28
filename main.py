fa92a88f6284acd6d58290b03d70349a5fbe48b1
MohithPothuru-git <mohith.p@algofusiontech.com>
2025-07-28 16:59:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from app import app
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+class Base(DeclarativeBase):
+    pass
+db = SQLAlchemy(model_class=Base)
+# create the app
+app = Flask(__name__)
+# setup a secret key, required by sessions
